405fb0291847e02025e444b673dd30a6888940db
Jamon Holmgren <jamon@infinite.red>
2018-11-19 11:42:48 -0800

    Added tests

--- plugin.js
+++ plugin.js
@@ -1,22 +1,22 @@
-const NPM_MODULE_NAME = 'react-native-webview'
+const NPM_MODULE_NAME = "react-native-webview";
 
 /**
  * Add the NPM package and configure it.
  */
-const add = async (context) => {
-  const { ignite } = context
+const add = async context => {
+  const { ignite } = context;
 
-  await ignite.addModule(NPM_MODULE_NAME, { version: '2.5.0', link: true })
+  await ignite.addModule(NPM_MODULE_NAME, { version: "2.5.0", link: true });
 
   // we need some sort of example, probably?
-}
+};
 
-const remove = async (context) => {
-  const { ignite } = context
+const remove = async context => {
+  const { ignite } = context;
 
-  await ignite.removeModule(NPM_MODULE_NAME, { unlink: true })
+  await ignite.removeModule(NPM_MODULE_NAME, { unlink: true });
 
   // remove the example?
-}
+};
 
-module.exports = { add, remove }
+module.exports = { add, remove };
---
+++ test.js
@@ -0,0 +1,31 @@
+/**
+ * Super simple tests.
+ */
+
+const NPM_NAME = "react-native-webview";
+
+const { add, remove } = require("./plugin.js");
+
+const assert = console.assert;
+
+assert(typeof add === "function", "Plugin should export an `add` function");
+assert(typeof remove === "function", "Plugin should export a `remove` fn");
+
+const mockedContext = {
+  ignite: {
+    addModule: async (modName, options) => {
+      // prettier-ignore
+      assert(modName === NPM_NAME, `Wrong NPM name in add function: ${modName}`);
+    },
+    removeModule: async (modName, options) => {
+      // prettier-ignore
+      assert(modName === NPM_NAME, `Wrong NPM name in remove function: ${modName}`);
+    }
+  }
+};
+
+// Test the functions with mocked context
+add(mockedContext).then(() => {});
+remove(mockedContext).then(() => {});
+
+console.info("Tests passed successfully.");
