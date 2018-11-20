9115542e99217bfd1b51edb064e704a5793b26b0
Jamon Holmgren <jamon@infinite.red>
2018-11-19 17:11:01 -0800

    fix(test): Testing a change to the code base & circleci deploy

--- plugin.js
+++ plugin.js
@@ -7,16 +7,12 @@ const add = async context => {
   const { ignite } = context;
 
   await ignite.addModule(NPM_MODULE_NAME, { version: "2.5.0", link: true });
-
-  // we need some sort of example, probably?
 };
 
 const remove = async context => {
   const { ignite } = context;
 
   await ignite.removeModule(NPM_MODULE_NAME, { unlink: true });
-
-  // remove the example?
 };
 
 module.exports = { add, remove };
